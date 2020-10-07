b41bfa7ecd31ffd72b9c91b8805581de8e6e00d6
Lucas Salmons <lucsalmons@gmail.com>
2020-10-06 23:06:07 -0700

    useEffect hook demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React, { useState } from "react";
-import { useForm } from "./useForm";
-
-function expensiveInitState() {
-  //large calculations here
-  return 10;
-}
+import React, { useEffect, useState } from "react";
+import { useForm } from "./hooks/useForm";
+import { useFetch } from "./hooks/useFetch";
+// import { Hello } from "./hello.component";
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   return (
     <div>
-      <input name="email" value={values.email} onChange={handleChange} />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>increment</button>
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
+      {showHello && <Hello />} */}
+      <>
+        <input
+          name="email"
+          placeholder="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
     </div>
   );
 };
