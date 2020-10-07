b553f6cf4f6cd67aa460631d01de4156bd774401
Lucas Salmons <lucsalmons@gmail.com>
2020-10-06 22:10:36 -0700

    useState hook demo

---
+++ src/useForm.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useForm = (initialValues) => {
+  const [values, setvalues] = useState(initialValues);
+
+  return [
+    values,
+    (e) => {
+      setvalues({
+        ...values,
+        [e.target.name]: e.target.value,
+      });
+    },
+  ];
+};
